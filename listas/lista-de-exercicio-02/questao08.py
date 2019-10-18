f6b2acfb93d825d9797c6701993b0e7fa7ffee2b
Jonathan C Santos <jonathanc8102@gmail.com>
2019-10-18 16:54:14 -0300

    Adicionando questões 8 e 9

---
+++ listas/lista-de-exercicio-02/questao08.py
@@ -0,0 +1,4 @@
+ganha=float(input("Valor da hora trabalhada: "))
+horas=int(input("Qauntidade de horas trabalhadas no mes: "))
+salario=ganha*horas
+print("Salario por mes: {}".format(salario))
---
+++ listas/lista-de-exercicio-02/questao09.py
@@ -0,0 +1,3 @@
+temp_fa=float(input("Digite a temperatura em Fahrenheit: "))
+cel=(5*(temp_fa-32)/9)
+print(cel)
